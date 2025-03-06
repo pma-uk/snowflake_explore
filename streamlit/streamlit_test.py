2565309b1377c31bf9cfceba2cfe5479b40fe6a9
pma-uk <pma.uk@proton.me>
2025-03-06 09:59:17 -0500

    Added simple title and query to fetch and display Customers table data.

--- streamlit/streamlit_test.py
+++ streamlit/streamlit_test.py
@@ -1,2 +1,13 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
+
+# Get current credentials
+session = get_active_session()
+
+# Title and description
+st.title("Streamlit in Snowflake Test")
+
+# Show Customers
+if st.button("Show Customers"):
+    rows = session.sql("SELECT * FROM CUSTOMERS LIMIT 10;").collect()
+    st.write(rows)

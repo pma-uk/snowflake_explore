390e404c2ef7a0863532251db415868d3e2ee523
pma-uk <pma.uk@proton.me>
2025-03-07 09:03:59 -0500

    Modified buttons to set current stage. Content displayed depends on current stage.

--- streamlit/streamlit_test.py
+++ streamlit/streamlit_test.py
@@ -7,16 +7,33 @@ session = get_active_session()
 # Title and description
 st.title("Streamlit in Snowflake Test")
 
+
 # Set up columns for buttons
-button_cols = st.columns([1,1,1]) # Array specifies relative width of columns (brings buttons closer together)
+button_cols = st.columns([1,1,1,1]) # Array specifies relative width of columns (brings buttons closer together)
+
+# Set stage to track what should be displayed
+# 0 - None; 1 - Customers; 2 - Orders; 3 - Products
+if 'stage' not in st.session_state:
+    st.session_state.stage = 0
+
 
 # Show Customers
 if button_cols[0].button("Show Customers"):
-    rows = session.sql("SELECT * FROM CUSTOMERS;").collect()
-    st.write(rows)
+    st.session_state.stage = 1
 
 # Show Orders
 if button_cols[1].button("Show Orders"):
+    st.session_state.stage = 2
+
+
+# Check the stage and display it correspondingly
+# Customers
+if st.session_state.stage == 1:
+    rows = session.sql("SELECT * FROM CUSTOMERS;").collect()
+    st.write(rows)
+
+# Orders
+if st.session_state.stage == 2:
     query = """
     SELECT O.ORDER_ID, C.NAME AS CUSTOMER, P.NAME AS PRODUCT, O.QUANTITY, O.TOTAL_PRICE, O.ORDER_DATE
     FROM ORDERS O

1a2eb31c20a4ff5b7904f08411a3ebb8c5ce9394
pma-uk <pma.uk@proton.me>
2025-03-07 08:49:49 -0500

    Modified button columns for formatting and changed 'Show Products' button to 'Show Orders' button which performs a join on all tables to display order information.

--- streamlit/streamlit_test.py
+++ streamlit/streamlit_test.py
@@ -8,14 +8,21 @@ session = get_active_session()
 st.title("Streamlit in Snowflake Test")
 
 # Set up columns for buttons
-button_cols = st.columns(2)
+button_cols = st.columns([1,1,1]) # Array specifies relative width of columns (brings buttons closer together)
 
 # Show Customers
 if button_cols[0].button("Show Customers"):
     rows = session.sql("SELECT * FROM CUSTOMERS;").collect()
     st.write(rows)
 
-# Show Products
-if button_cols[1].button("Show Products"):
-    rows = session.sql("SELECT * FROM PRODUCTS;").collect()
+# Show Orders
+if button_cols[1].button("Show Orders"):
+    query = """
+    SELECT O.ORDER_ID, C.NAME AS CUSTOMER, P.NAME AS PRODUCT, O.QUANTITY, O.TOTAL_PRICE, O.ORDER_DATE
+    FROM ORDERS O
+    JOIN CUSTOMERS C ON C.ID = O.CUSTOMER_ID
+    JOIN PRODUCTS P ON P.PRODUCT_ID = O.PRODUCT_ID
+    ORDER BY O.ORDER_DATE DESC;
+    """
+    rows = session.sql(query).collect()
     st.write(rows)
